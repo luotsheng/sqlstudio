6c1db3c1c76864610ef06ed16003697690c70298
luotiansheng <2968640488@qq.com>
2026-03-16 13:03:11 +0800

    [工作窗口] 新增 sql 编辑器组件

--- src/main/java/com/changhong/sqlstudio/SqlStudio.java
+++ src/main/java/com/changhong/sqlstudio/SqlStudio.java
@@ -1,20 +1,25 @@
 package com.changhong.sqlstudio;
 
-import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.*;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+
+import java.util.ArrayList;
 
 import static org.eclipse.swt.SWT.*;
 
 public class SqlStudio {
 
     private static final String SQL_STUDIO_TITLE = "数据库可视化管理工具";
-    private static final int[] SQL_STUDIO_RATIO = new int[] {30, 70};
+    private static final int[] SQL_STUDIO_RATIO = new int[] {25, 75};
 
     public static final Display DISPLAY = new Display();
     private final Shell shell;
@@ -132,7 +137,84 @@ public class SqlStudio {
     private void createRightComposite(SashForm sashForm) {
         Composite site = new Composite(sashForm, NONE);
         site.setLayout(new FillLayout());
-        new Table(site, FULL_SELECTION);
+
+        CTabFolder tabFolder = new CTabFolder(site, HORIZONTAL | CLOSE);
+
+        CTabItem tabItem = new CTabItem(tabFolder, NONE);
+        tabItem.setText("新建查询");
+
+        StyledText sqlEditor = new StyledText(tabFolder, MULTI | BORDER | V_SCROLL | H_SCROLL);
+        tabItem.setControl(sqlEditor);
+
+        sqlEditor.setBackground(DISPLAY.getSystemColor(COLOR_WHITE));
+        sqlEditor.setForeground(DISPLAY.getSystemColor(COLOR_BLACK));
+
+        Font font = new Font(DISPLAY, new FontData[] {
+                new FontData("Microsoft YaHei UI", 12, NORMAL),
+                new FontData("Consolas", 12, NORMAL),
+                new FontData("Monaco", 12, NORMAL),
+                new FontData("Courier New", 12, NORMAL)
+        });
+
+        sqlEditor.setFont(font);
+
+        sqlEditor.addLineStyleListener(new LineStyleListener() {
+            private final String[] keywords = {
+                    "SELECT", "FROM", "WHERE", "INSERT", "UPDATE", "DELETE", "JOIN", "ON", "INNER", "LEFT", "RIGHT", "FULL", "OUTER",
+                    "AND", "OR", "NOT", "AS", "BY", "GROUP", "HAVING", "ORDER", "ASC", "DESC", "LIMIT", "OFFSET",
+                    "CREATE", "TABLE", "DROP", "ALTER", "ADD", "COLUMN", "PRIMARY", "KEY", "FOREIGN", "REFERENCES",
+                    "VALUES", "INTO", "SET", "DISTINCT", "ALL", "UNION", "INTERSECT", "EXCEPT", "CASE", "WHEN", "THEN", "ELSE", "END",
+                    "NULL", "IS", "LIKE", "BETWEEN", "EXISTS", "IN"
+            };
+            private final Color keywordColor = DISPLAY.getSystemColor(COLOR_DARK_BLUE);
+            private final Color stringColor   = DISPLAY.getSystemColor(COLOR_DARK_GREEN);
+
+            @Override
+            public void lineGetStyle(LineStyleEvent event) {
+                String lineUpper = event.lineText.toUpperCase();
+                java.util.List<StyleRange> styles = new ArrayList<>();
+
+                for (String kw : keywords) {
+                    int pos = 0;
+                    while ((pos = lineUpper.indexOf(kw, pos)) >= 0) {
+                        boolean isWordStart = pos == 0 || !Character.isJavaIdentifierPart(event.lineText.charAt(pos - 1));
+                        boolean isWordEnd = pos + kw.length() == event.lineText.length() ||
+                                !Character.isJavaIdentifierPart(event.lineText.charAt(pos + kw.length()));
+
+                        if (isWordStart && isWordEnd) {
+                            StyleRange sr = new StyleRange();
+                            sr.start = pos + event.lineOffset;
+                            sr.length = kw.length();
+                            sr.foreground = keywordColor;
+                            sr.fontStyle = BOLD;
+                            styles.add(sr);
+                        }
+                        pos += kw.length();
+                    }
+                }
+
+                int start = -1;
+                for (int i = 0; i < event.lineText.length(); i++) {
+                    char c = event.lineText.charAt(i);
+                    if (c == '\'') {
+                        if (start == -1) {
+                            start = i;
+                        } else {
+                            StyleRange sr = new StyleRange();
+                            sr.start = start + event.lineOffset;
+                            sr.length = i - start + 1;
+                            sr.foreground = stringColor;
+                            styles.add(sr);
+                            start = -1;
+                        }
+                    }
+                }
+
+                if (!styles.isEmpty()) {
+                    event.styles = styles.toArray(new StyleRange[0]);
+                }
+            }
+        });
     }
 
     private void run0(String[] args) {
