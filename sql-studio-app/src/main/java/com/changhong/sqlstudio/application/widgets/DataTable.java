edb6c505926c612cd0df9fcc5df96e00329e7e7c
luotiansheng <2968640488@qq.com>
2026-03-18 22:48:36 +0800

    [项目文档] 新增 README.md 文件

--- sql-studio-app/src/main/java/com/changhong/sqlstudio/application/widgets/DataTable.java
+++ sql-studio-app/src/main/java/com/changhong/sqlstudio/application/widgets/DataTable.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
  * @author Luo Tiansheng
  * @since 2026-03-01
  */
-public class GridViewer extends Composite
+public class DataTable extends Composite
 {
         private final Grid grid;
         private final NNTable tableNode;
@@ -33,7 +33,7 @@ public class GridViewer extends Composite
         private int count = 50;
         private QueryResultSet queryResultSet;
 
-        public GridViewer(Composite parent, NNTable tableNode)
+        public DataTable(Composite parent, NNTable tableNode)
         {
                 super(parent, SWT.NONE);
                 setLayout(new FillLayout());
