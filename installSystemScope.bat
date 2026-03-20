41ce71b3d947b83c0b205fcf5260bf16411d9fd9
luotiansheng <2968640488@qq.com>
2026-03-20 19:28:47 +0800

    [项目构建] 新增 windows 安装 maven 依赖脚本

---
+++ installSystemScope.bat
@@ -0,0 +1,5 @@
+mvn install:install-file -Dfile=com.changhong.sqlstudio/lib/org.eclipse.nebula.widgets.grid_1.2.1.202601240135.jar ^
+    -DgroupId=org.eclipse.nebula ^
+    -DartifactId=org.eclipse.nebula.widgets.grid ^
+    -Dversion=1.2.1.202601240135 ^
+    -Dpackaging=jar
